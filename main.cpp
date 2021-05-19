b8538d0214cd22d4867d2da1a8a1dedc97e577d3
Maksim Zotikov <71289146+MaxZotik@users.noreply.github.com>
2021-05-20 00:56:23 +0300

    edited methods main, equalsTwoNumber, equalsNumber, substrEndZero and substrStartZero deleted methods substrEnd and substrStart

--- main.cpp
+++ main.cpp
@@ -16,35 +16,15 @@ bool correctNumber(std::string numberReal){
 	return result;
 }
 
-std::string substrStart(std::string str){
-    std::string strTemp;
-    for(int i = 0; i < str.length(); i++){
-        if(str[0] == '.' || (str[0] == '-' && str[1] == '.')){
-            strTemp += '0';
-            return strTemp;
-        }
-        if(str[i] == '.'){
-            return strTemp;
-        }
-
-        strTemp += str[i];
-    }
-
-    return strTemp;
-}
-
-//
 std::string substrStartZero(std::string str){
     std::string strTemp;
     bool zero = true;
 
     for(int i = 0; i < str.length(); i++){
         if(zero && str[0] == '-'){
-            std::cout << "if(str[0] == '-')" << std::endl;
             strTemp += str[0];
             zero = false;
         }else if(str[i] != '0'){
-            std::cout << "else if(str[i] != '0')" << std::endl;
             strTemp += str.substr(i, str.length());
             break;
         }
@@ -53,33 +33,12 @@ std::string substrStartZero(std::string str){
     return strTemp;
 }
 
-//
 std::string substrEndZero(std::string str){
     std::string strTemp;
-    std::cout << str << "\n";
 
     for(int i = str.length() - 1; i >= 0; i--){
-        std::cout << "for(int i = str.length() - 1; i > 0; i--) " << i << "\n";
         if(str[i] != '0'){
             strTemp = str.substr(0, i + 1);
-            std::cout << "if(str[i] == '0') " << strTemp << "\n";
-        }
-    }
-
-    return strTemp;
-}
-
-std::string substrEnd(std::string str){
-    bool point = false;
-    std::string strTemp;
-
-    for(int i = 0; i < str.length(); i++){
-        if(str[0] == '0' || str[i] == '.'){
-            point = true;
-        }
-
-        if(point){
-            strTemp += str[i];
         }
     }
 
@@ -91,13 +50,8 @@ int equalsTwoNumber(std::string strOne, std::string strTwo){
     //1 - more, 0 - equal, -1 - less
 
     if(strOne.length() > strTwo.length()){
-
-        std::cout << "if(substrStartZero(strOne).length() > substrStartZero(strTwo).length())" << "\n";
-        std::cout << strOne << strTwo << "\n";
         return 1;
     }else if(strOne.length() < strTwo.length()){
-        std::cout << "if(substrStartZero(strOne).length() < substrStartZero(strTwo).length())" << "\n";
-        std::cout << strOne << strTwo << "\n";
         return -1;
     }else{
         for(int i = 0; i < strOne.length() || i < strTwo.length(); i++){
@@ -110,27 +64,6 @@ int equalsTwoNumber(std::string strOne, std::string strTwo){
         }
     }
 
-/*
-    if(substrEndZero(strOne).length() > substrEndZero(strTwo).length()){
-        return 1;
-    }
-
-    if(substrEndZero(strOne).length() < substrEndZero(strTwo).length()){
-        return -1;
-    }
-
-    if(substrEndZero(strOne).length() == substrEndZero(strTwo).length()){
-
-        for(int i = 0; i < substrEndZero(strOne).length(); i++){
-            if(substrEndZero(strOne)[i] < substrEndZero(strTwo)[i]){
-                return -1;
-            }
-            if(substrEndZero(strOne)[i] > substrEndZero(strTwo)[i]){
-                return 1;
-            }
-        }
-    }
-*/
     return 0;
 }
 
@@ -146,9 +79,6 @@ std::string equalsNumber(std::string strOne, std::string strTwo){
     strOneRight = substrEndZero((oneIndexPoint != -1) ? strOne.substr(oneIndexPoint + 1) : "");
     strTwoLeft = substrStartZero((twoIndexPoint != -1) ? strTwo.substr(0, twoIndexPoint) : strTwo);
     strTwoRight = substrEndZero((twoIndexPoint != -1) ? strTwo.substr(twoIndexPoint + 1) : "");
-
-    std::cout << ((oneIndexPoint != -1) ? strOne.substr(0, oneIndexPoint) : strOne) << " " << ((oneIndexPoint != -1) ? strOne.substr(oneIndexPoint + 1) : "") << " " << ((twoIndexPoint != -1) ? strTwo.substr(0, twoIndexPoint) : strTwo) << " " << ((twoIndexPoint != -1) ? strTwo.substr(twoIndexPoint + 1) : "") << "\n";
-    std::cout << strOneLeft << " " << strOneRight << " " << strTwoLeft << " " << strTwoRight << "\n";
 
     if(strOne[0] == '-' && strTwo[0] != '-'){
         return less;
@@ -174,10 +104,9 @@ std::string equalsNumber(std::string strOne, std::string strTwo){
 
 int main() {
 
-    std::string numberOne = "00100.110";
-    std::string numberTwo = "100.1";
+    std::string numberOne;
+    std::string numberTwo;
 
-    /*
     for(;;){
         std::cout << "Enter real number one: ";
         std::cin >> numberOne;
@@ -190,25 +119,6 @@ int main() {
             break;
         }
     }
-*/
-    if (!correctNumber(numberOne) || !correctNumber(numberTwo)) {
-        std::cout << "invalid number entered \n";
-    }
 
-    std::cout << substrStart(numberOne) << std::endl;
-    std::cout << std::endl;
 	std::cout << equalsNumber(numberOne, numberTwo) << std::endl;
-
-	/*
-	int i = numberTwo.find('.');
-	std::cout << numberTwo.substr(0, i) << std::endl;
-    std::cout << numberTwo.substr(i + 1) << std::endl;
-
-    int a = numberOne.find('.');
-    std::cout << a << std::endl;
-    std::cout << substrStartZero(numberOne) << std::endl;
-    std::cout << substrEndZero(numberTwo) << std::endl;
-    */
-
-
 }
